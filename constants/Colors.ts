cfdd9db518a18e7d2496fb15dc034df4c6b1384d
matin <kingman.mza@gmail.com>
2023-04-19 19:52:17 +0330

    add all

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,26 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#fff";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
     tint: tintColorLight,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: "#fff",
+    background: "#000",
     tint: tintColorDark,
-    tabIconDefault: '#ccc',
+    tabIconDefault: "#ccc",
     tabIconSelected: tintColorDark,
   },
+  grey100: "#EAEAE9",
+  grey200: "#BCABA2",
+  grey300: "#717F8B",
+  cream100: "#E4CBA8",
+  cream200: "#D39465",
+  primary: "#FE2C21",
+  main: "#3A383C",
 };
